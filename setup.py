629fb42bcb22308be0ef9bee5647546a564b42b2
Tobi DEGNON <tobidegnon@protonmail.com>
2021-05-14 13:37:36 +0100

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Tobi-De/qosic-sdk",
-    version="1.0.0",
+    version="1.0.1",
     zip_safe=False,
 )
