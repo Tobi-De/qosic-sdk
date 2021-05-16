0a3362a32f157fef94ff70b7ad6bcca6bf817432
Tobi DEGNON <tobidegnon@protonmail.com>
2021-05-16 10:54:05 +0100

    Bump version: 1.0.1 → 1.0.2

--- qosic/__init__.py
+++ qosic/__init__.py
@@ -10,4 +10,4 @@ from .models import (  # noqa
 
 __author__ = """Tobi DEGNON"""
 __email__ = "degnonfrancis@gmail.com"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Tobi-De/qosic-sdk",
-    version="1.0.1",
+    version="1.0.2",
     zip_safe=False,
 )
