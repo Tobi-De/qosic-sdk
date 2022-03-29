4d99c1fb4bc75df286cc5e10b64224c752ea9752
Tobi-De <tobidegnon@protonmail.com>
2022-03-29 11:32:58 +0100

    remove author and version from __init__

--- qosic/__init__.py
+++ qosic/__init__.py
@@ -2,7 +2,3 @@
 from .api import Client  # noqa
 from .providers import MTN, MOOV  # noqa
 from .utils import Result, Payer  # noqa
-
-__author__ = """Tobi DEGNON"""
-__email__ = "degnonfrancis@gmail.com"
-__version__ = "3.0.0"
